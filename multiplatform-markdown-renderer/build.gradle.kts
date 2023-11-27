65ad3643b080dea8995e8cbd153a81d09fcad127
Steven Roebert <steven@roebert.nl>
2023-11-27 11:13:31 +0100

    Updated gradle

--- multiplatform-markdown-renderer/build.gradle.kts
+++ multiplatform-markdown-renderer/build.gradle.kts
@@ -52,8 +52,6 @@ android {
 }
 
 kotlin {
-    targetHierarchy.default()
-
     targets.all {
         compilations.all {
             compilerOptions.configure {
@@ -66,59 +64,9 @@ kotlin {
     androidTarget {
         publishLibraryVariants("release")
     }
-//    jvm {
-//        compilations {
-//            all {
-//                kotlinOptions.jvmTarget = "11"
-//            }
-//        }
-//
-//        testRuns["test"].executionTask.configure {
-//            useJUnit {
-//                excludeCategories("org.intellij.markdown.ParserPerformanceTest")
-//            }
-//        }
-//    }
-//    js(IR) {
-//        nodejs()
-//    }
-//    macosX64()
-//    macosArm64()
-//    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val fileBasedTest by creating {
-            dependsOn(commonTest)
-        }
-        val nativeMain by getting {
-            dependsOn(commonMain)
-        }
-        val nativeTest by getting {
-            dependsOn(fileBasedTest)
-        }
-        val nativeSourceSets = listOf(
-            "macosX64",
-            "macosArm64",
-            "ios",
-            "iosSimulatorArm64"
-        ).map { "${it}Main" }
-        for (set in nativeSourceSets) {
-            getByName(set).dependsOn(nativeMain)
-        }
-        val nativeTestSourceSets = listOf(
-            "macosX64",
-            "macosArm64"
-        ).map { "${it}Test" }
-        for (set in nativeTestSourceSets) {
-            getByName(set).dependsOn(nativeTest)
-            getByName(set).dependsOn(fileBasedTest)
-        }
     }
 }
 
